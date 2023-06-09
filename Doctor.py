b12a4bca31f9f3ee69faea608327e1a3a2124eed
DustLi-DC <128945594+DustLi-DC@users.noreply.github.com>
2023-06-09 12:59:56 +0800

    Create Doctor.py

---
+++ Doctor.py
@@ -0,0 +1,13 @@
+import time
+
+def pomodoro_timer(minutes):
+    seconds = minutes * 60
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+    
+pomodoro_timer(25) # Set the number of minutes for the timer
